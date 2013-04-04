02762eee86d9e172407bee10cf68f846b8555240
Joe Barbere <joe.barbere@gmail.com>
2013-04-04 18:57:25 -0400

    Changed the buffer to a minimum value. Added debugging output.

--- src/Listen.java
+++ src/Listen.java
@@ -1,6 +1,5 @@
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.sound.sampled.TargetDataLine;
 
@@ -12,16 +11,24 @@ public class Listen implements Runnable {
 	
 	@Override
 	public void run() {
-		OutputStream out = new ByteArrayOutputStream();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
 
 		try {
 		    while (running) {
-		        byte[] buffer = new byte[1024];
+		        byte[] buffer = new byte[4];
 		        int count = line.read(buffer, 0, buffer.length);
 		        if (count > 0) {
 		            out.write(buffer, 0, count);
 		        }
 		    }
+		    
+		    // debugging output
+		    byte[] byteArray = out.toByteArray();
+		    System.out.println(byteArray.length);
+		    for(int i= 0 ; i < byteArray.length; i++) {
+		    	System.out.println(byteArray[i]);
+		    }
+
 		    out.close();
 		} catch (IOException e) {
 		    System.err.println("I/O problems: " + e);
